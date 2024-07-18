31f5b3740f8092b7edaa54a70141cfd0656ac9bd
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-07-18 12:06:05 +0200

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+############ COLOURED BASH TEXT
+
+# ANSI color codes
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+PURPLE='\033[0;35m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+
+################################################################################################## FILE & FOLDER PATHS
+
+# Location
+APPLICATION="arch-suite"
+BASE="$HOME/bash.$APPLICATION"
+FILES="$BASE/files/fastfetch"
+CONFDIR="$HOME/.config/fastfetch"
+
+
+################################################################################################## FILE & FOLDER PATHS
+
+update_fastfetch() {
+    # Generate a new Fastfetch config
+    fastfetch --gen-config-force
+
+    # Backup existing config
+    mv "$CONFDIR/config.jsonc" "$CONFDIR/config.jsonc.bak"
+
+    # Copy the new Fastfetch configurations
+    cp -R $FILES/* "$CONFDIR/"
+
+    echo "Fastfetch configuration updated successfully."
+}
+
+
+################################################################################################## FILE & FOLDER PATHS
+
+# Change FASTFETCH Output
+update_fastfetch
